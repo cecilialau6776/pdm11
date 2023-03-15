cf0078b67df39316ab3d4e24842a33010e2adcd9
Damon Gonzalez <dbg3220@rit.edu>
2023-03-15 09:00:25 -0400

    Created an interface for communication between the UI and app, it is expected more methods will be added to the interface

---
+++ src/app/IApp.java
@@ -0,0 +1,37 @@
+package app;
+
+/**
+ * Used by UI to interface to the application
+ */
+public interface IApp {
+
+    /**
+     * Tells the caller whether a user is logged in on this application
+     * @return true if a user is logged in, false if not
+     */
+    boolean isUserLoggedIn();
+
+    /**
+     * Attempts to log a user into this application with the given username
+     * and password
+     * @param username The username of the user
+     * @param password The password of the user
+     * @return true if the user was successfully logged in, false if
+     * authentication was unsuccessful or there is currently another user
+     * logged in
+     */
+    boolean logIn(String username, String password);
+
+    /**
+     * Logs out the current user. If there is no user logged in than it does
+     * nothing.
+     */
+    void logOut();
+
+    /**
+     * Closes application and all relevant resources
+     */
+    void exit();
+
+    //TODO add more methods as needed
+}
